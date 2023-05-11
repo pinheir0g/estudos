70a92f81e97c6f6b225b840d0bdc468b25178c02
pinheir0g <gv.pinheiro@outlook.com>
2023-05-11 19:25:31 -0300

    Testando

--- exercicios_ADS/calculador_simples.py
+++ exercicios_ADS/calculador_simples.py
@@ -1,3 +1,5 @@
 # Escreva um algoritmo que leia dois valores numéricos e que pergunte ao
 # usuário qual operação matematica ele deseja realizar.
 # Exiba na tela o resultado da operação.
+
+print('Hello, World!')
