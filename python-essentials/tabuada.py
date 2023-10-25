3f5a3d9a5b8b53488b33702f16cbabaf2094ce85
pinheir0g <gv.pinheiro@outlook.com>
2023-10-25 17:59:38 -0300

    New version tabuada.py

--- python-essentials/tabuada.py
+++ python-essentials/tabuada.py
@@ -9,12 +9,13 @@ __author__ = "Gustavo"
 
 # numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# Iterable
 numeros = list(range(1, 11))
 
-# Iterable
-
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("---------------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} X {n2} = {resultado}"))
+    print("#"*18)
