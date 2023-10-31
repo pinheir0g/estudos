0cb242c08b6fe340bb0fab82e78c8ea4a4dd376e
pinheir0g <gv.pinheiro@outlook.com>
2023-10-31 11:08:10 -0300

    Update hello.py

--- python-essentials/hello.py
+++ python-essentials/hello.py
@@ -9,16 +9,48 @@ Have the LANG variable properly configured
 Ex:
     export LANG=pt_BR
 
+Ou informe atraves do CLI argument `--lang`
+
+Ou o usuário terá que digitar.
+
 Execution:
     python3 hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Gustavo Pinheiro"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+
+arguments = {"lang": None, "count": 1}
+
+for arg in sys.argv[1:]:
+     # TODO: Tratar ValueError
+
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+
+    if key not in arguments:
+        print(f"Invalid Option: `{key}`")
+        sys.exit()
+
+    arguments[key] = value
+
+
+current_language =  arguments["lang"]
+
+if current_language is None:
+    # TODO: Usar repetição
+    
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language: ")
+
+current_language = current_language[:5]
 
 msg = {"en_US": "Hello, World!",
        "pt_BR": "Olá Mundo!",
@@ -31,4 +63,6 @@ msg = {"en_US": "Hello, World!",
        }
 
 #  O(1) - constante
-print(msg[current_language])
+print(
+    msg[current_language] * int(arguments["count"])
+    )
